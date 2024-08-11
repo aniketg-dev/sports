027bda7df9028809b25a136c8724aa109e039e0a
AniketGX38 <aniket.gangurde@projectx38.com>
2024-08-11 17:13:37 +0530

    changes

---
+++ js/custome.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    document.querySelectorAll('.fade-in').forEach(section => {
+        observer.observe(section);
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ['./pages/**/*.{html,js}'],
     theme: {
-      extend: {},
+      extend: {
+        height: {
+        '20vh':'20vh',
+        '90vh': '90vh',
+        '100vh': '100vh',
+        '120vh': '120vh', // Add more as needed
+      },},
     },
     plugins: [],
   }
